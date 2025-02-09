9090c5c93e6a858114cebc7fa2dd042424875422
heronhoga <nozzeravixer@gmail.com>
2025-02-09 14:50:28 +0700

    feat:add update function for laptop

--- src/routes/laptop/handlers.ts
+++ src/routes/laptop/handlers.ts
@@ -43,3 +43,33 @@ export async function createLaptop({ body, set, error }: any) {
     return "Internal server error!";
   }
 }
+
+export async function updateLaptop({ params: { id }, body, set, error }: any) {
+  try {
+    const newLaptop = body;
+    //find old data
+    const oldLaptopData: any[] =
+      await db.$queryRaw`SELECT * from "Laptop" where id_laptop = ${id}`;
+    if (oldLaptopData.length === 0) {
+      return error(404, "Laptop not found");
+    }
+
+    const updatedLaptop = {
+      brand: newLaptop.brand ? newLaptop.brand : oldLaptopData[0].brand,
+      description: newLaptop.description
+        ? newLaptop.description
+        : oldLaptopData[0].description,
+      price: newLaptop.price ? newLaptop.price : oldLaptopData[0].price,
+    };
+
+    //update laptop data
+    const updateLaptop =
+      await db.$queryRaw`UPDATE "Laptop" set brand = ${updatedLaptop.brand}, description = ${updatedLaptop.description}, price = ${updatedLaptop.price} WHERE id_laptop = ${id} RETURNING *`;
+
+    return updateLaptop;
+  } catch (error) {
+    console.log(error);
+    set.status(500);
+    return "Internal server error";
+  }
+}
--- src/routes/laptop/index.ts
+++ src/routes/laptop/index.ts
@@ -1,11 +1,11 @@
 import Elysia from "elysia";
-import { createLaptop, getLaptops, getSpecificLaptop } from "./handlers";
+import { createLaptop, getLaptops, getSpecificLaptop, updateLaptop } from "./handlers";
 
 const laptopRoutes = new Elysia({ prefix: "/laptop" })
   .get("/", getLaptops) 
   .get("/:id", getSpecificLaptop)
   .post("/", createLaptop)
-  .patch("/:id", () => "update specific laptop data")
+  .patch("/:id", updateLaptop)
   .delete("/:id", () => "delete specific laptop data");
 
 export default laptopRoutes;
